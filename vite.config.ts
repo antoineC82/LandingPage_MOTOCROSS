703d7939f65a67f8e7195c10e284ee4e4a428798
antoineC82 <antoineculotpro@gmail.com>
2025-03-19 11:23:21 +0100

    okok

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/LandingPage_MOTOCROSS/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
